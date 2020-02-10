202ccf42cd95719635ca3b96ac06c836bc688239
Matthew VH <vanheckematt@gmail.com>
2020-02-09 18:45:22 -0600

    Created method for robot attacking dinosaur. Still need to create methods that will remove dinosaur/robot from game when their health reaches zero.

--- robotsVsDinosaurs/Battlefield.cs
+++ robotsVsDinosaurs/Battlefield.cs
@@ -127,8 +127,8 @@ namespace robotsVsDinosaurs
                 //Create attack and defense values (weighted by attack power and current energy/power level and multiplied by dice roll)
                 int attackValue = attackerDiceRoll * dinosaur.dinosaurAttackPower * dinosaur.dinosaurEnergy;
                 int defenseValue = defenderDiceRoll * robot.weapon.weaponAttackPower * robot.robotPowerLevel;
-                Console.WriteLine(attackValue);
-                Console.WriteLine(defenseValue);
+                Console.WriteLine($"Dinosaur scores {attackValue}!");
+                Console.WriteLine($"Robot scores {defenseValue}!");
                 Console.WriteLine($"{dinosaur.dinosaurName} has {dinosaur.dinosaurHealth} health and {dinosaur.dinosaurEnergy} energy remaining");
                 Console.WriteLine($"{robot.robotName} has {robot.robotHealth} health and {robot.robotPowerLevel} power remaining");
                 //See whose fight value wins the battle. Tie goes to the defender. Loser loses 5 health points. Both lose 2 energy points.
@@ -161,8 +161,49 @@ namespace robotsVsDinosaurs
             } while (attackAgain);
         }
         //public void BuildDinosaurList()
-        //{
-        //    dinosaurList = myHerd.GenerateDinosaurList();
-        //}
+        public void RobotAttacksDinosaur(Robot robot, Dinosaur dinosaur)
+        {
+            bool attackAgain = false;
+            do
+            {
+                //Roll two dice for attacker and two for defender
+                int attackerDiceRoll = random.Next(2, diceRollMaxValue);
+                int defenderDiceRoll = random.Next(2, diceRollMaxValue);
+                //Create attack and defense values (weighted by attack power and current energy/power level and multiplied by dice roll)
+                int attackValue = attackerDiceRoll * robot.weapon.weaponAttackPower * robot.robotPowerLevel;
+                int defenseValue = defenderDiceRoll * dinosaur.dinosaurAttackPower * dinosaur.dinosaurEnergy;
+                Console.WriteLine($"Robot scores {attackValue}!");
+                Console.WriteLine($"Dinosaur scores {defenseValue}!");
+                Console.WriteLine($"{dinosaur.dinosaurName} has {dinosaur.dinosaurHealth} health and {dinosaur.dinosaurEnergy} energy remaining");
+                Console.WriteLine($"{robot.robotName} has {robot.robotHealth} health and {robot.robotPowerLevel} power remaining");
+                //See whose fight value wins the battle. Tie goes to the defender. Loser loses 5 health points. Both lose 2 energy points.
+                if (attackValue > defenseValue)
+                {
+                    dinosaur.dinosaurHealth -= 5;
+                    Console.WriteLine("Robot " + robot.robotName + " wins the battle!");
+                }
+                else
+                {
+                    robot.robotHealth -= 5;
+                    Console.WriteLine("Diniosaur " + dinosaur.dinosaurName + " wins the battle!");
+                }
+                robot.robotPowerLevel -= 2;
+                dinosaur.dinosaurEnergy -= 2;
+                //Ask user if robot wants to attack again.
+                Console.WriteLine($"To have {robot.robotName} attack again, press enter. To have him back down, hit any other key. A rest will cause both participants to regain 2 energy.");
+                ConsoleKeyInfo keyInput = Console.ReadKey();
+                switch (keyInput.Key)
+                {
+                    case ConsoleKey.Enter:
+                        attackAgain = true;
+                        break;
+                    default:
+                        robot.robotPowerLevel += 2;
+                        dinosaur.dinosaurEnergy += 2;
+                        attackAgain = false;
+                        break;
+                }
+            } while (attackAgain);
+        }
     }
 }
